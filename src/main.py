20ecbaf5c909c4323ee1f2fea2b4ddc4e9977f1b
thanhphuong163 <thanhphuong.its@gmail.com>
2024-12-23 00:55:08 -0700

    preprocessed videos to extract keypoint features

--- src/main.py
+++ src/main.py
@@ -1,12 +1,11 @@
 import hydra
+import logging
 from omegaconf import DictConfig, OmegaConf
 
 
-@hydra.main(version_base="1.3", config_path="../configs" , config_name="config")
+@hydra.main(version_base="1.3", config_path="../configs", config_name="config")
 def main(cfg: DictConfig):
-    print("Hello from uv-demo!")
-    print("My name is Phuong.")
-    print(f"{OmegaConf.to_yaml(cfg)}")
+    logging.info(f"\n{OmegaConf.to_yaml(cfg)}")
 
 
 if __name__ == "__main__":
